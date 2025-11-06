5fa5ffe96c099d6f5b5384ef4094a42368c2f8c4
Anders0nlima <andersoncon85@gmail.com>
2025-11-06 17:50:22 -0300

    crud feito

--- index.js
+++ index.js
@@ -1,9 +1,80 @@
 import express from "express"
+import { prisma } from "./src/db.js"
 
 const app = express()
+app.use(express.json())
 
-app.get("/",  (req, res) => {
-    res.json("Ola Mundo")
+app.get("/", async function(req, res) {
+    try{
+        const users = await prisma.veiculos.findMany();
+        res.json(users)
+    } catch(error){
+        console.log(error)
+    }
+    
+})
+
+app.get("/:id", async (req, res) => {
+    const veiculosId = req.params.id
+
+    const veiculo = await prisma.veiculos.findFirst({
+        where: {
+            id: veiculosId
+        }
+    })
+
+    console.log(veiculosId)
+    res.json(veiculo)
+
+})
+
+app.post("/cadatrar", async (req, res) => {
+    const { marca, modelo } = req.body
+
+    if(!modelo){
+        res.json({mensagem: "O modelo do veiculo é obrigatorio"})
+        return
+    }
+
+    const veiculo = await prisma.veiculos.create({
+        data: {
+            modelo: modelo,
+            marca: marca
+        }
+    })
+
+    res.json(veiculo)
+
+})
+
+app.put("/:id", async (req, res) => {
+    const{id} = req.params
+    const {modelo, marca} = req.body
+
+    if(!modelo){
+        res.json({mensagem: "O modelo do veiculo é obrigatorio"})
+        return
+    }
+
+    const veiculo = await prisma.veiculos.update({
+        where: {id: id},
+        data: { modelo: modelo, marca: marca }
+    })
+
+    res.json(veiculo)
+})
+
+app.delete("/:id", async(req, res) => {
+    const {id} = req.params
+
+    const veiculo = await prisma.veiculos.delete({
+        where: {id: id}
+    })
+
+    res.json({
+        mensagem: "veiculo excluido com sucesso",
+        veiculo
+    })
 
 })
 
--- src/db.js
+++ src/db.js
@@ -1,10 +1,11 @@
 import { PrismaClient } from "../generated/prisma/index.js";
 
-const prisma = new PrismaClient();
+export const prisma = new PrismaClient();
 
-async function getUser() {
+{/*async function getUser() {
     const user = await prisma.veiculos.findMany()
     console.log(user)
 }
 
-getUser()
+getUser()*/}
+
